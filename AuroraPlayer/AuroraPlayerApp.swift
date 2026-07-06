3948b0895aedfeeb1c1c78fb405aa8c4560442df
prabh <Prabhtajbrar7@gmail.com>
2026-07-06 14:13:24 +0530

    Initial Commit

---
+++ AuroraPlayer/AuroraPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuroraPlayerApp.swift
+//  AuroraPlayer
+//
+//  Created by Prabh's MacBook Pro 💻  on 06/07/26.
+//
+
+import SwiftUI
+
+@main
+struct AuroraPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuroraPlayerTests/AuroraPlayerTests.swift
@@ -0,0 +1,18 @@
+//
+//  AuroraPlayerTests.swift
+//  AuroraPlayerTests
+//
+//  Created by Prabh's MacBook Pro 💻  on 06/07/26.
+//
+
+import Testing
+
+struct AuroraPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
